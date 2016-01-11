27479f5e74de9f04e2301c3da13a5f99397bf0b4
Tom McKenna <tommckenna2000@yahoo.com>
2016-01-11 09:07:47 -0500

    get rekt

---
+++ Whatever/Whatever/JD.cs
@@ -0,0 +1,18 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatever
+{
+    class JD
+    {
+        Image jd = new Image("jd.png");
+        public JD(int x, int y) : base(x,y)
+        {
+
+        }
+    }
+}
